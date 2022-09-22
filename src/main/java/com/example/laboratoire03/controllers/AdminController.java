a87413afb9c4e379ac10408fd3fc22d27a5b720b
mauricefhebert <filiatreaultmaurice@gmail.com>
2022-09-21 22:21:26 -0400

    update

--- src/main/java/com/example/laboratoire03/controllers/AdminController.java
+++ src/main/java/com/example/laboratoire03/controllers/AdminController.java
@@ -1,7 +1,9 @@
 package com.example.laboratoire03.controllers;
 
-import com.example.laboratoire03.models.*;
+import com.example.laboratoire03.models.Admin;
+import com.example.laboratoire03.models.Cours;
 import com.example.laboratoire03.models.Error;
+import com.example.laboratoire03.models.Etudiant;
 import com.example.laboratoire03.services.AppDataContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,35 +28,11 @@ public class AdminController {
         this.dataContext = dataContext;
     }
 
-    private Admin adminSession;
-    private Admin getAdmin(HttpSession session) throws Exception {
-        try {
-            adminSession = new Admin();
-            if(session.getAttribute("admin") != null) {
-                adminSession = (Admin)session.getAttribute("admin");
-            } else {
-                session.setAttribute("admin", null);
-            }
-            return adminSession;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     @GetMapping("/admin")
     public ModelAndView admin(Model model, HttpServletRequest request, HttpSession session) {
         try {
             model.addAttribute("model", new Admin());
-
-            if(getAdmin(session) != null) {
-                adminSession = getAdmin(session);
-                return new ModelAndView("adminPanelFormEtudiant", "etudiant", new Etudiant());
-            }
-
-            session.setAttribute("admin", new Admin());
-
             return new ModelAndView("admin");
-
         } catch (Exception e) {
             return new ModelAndView("error", "model", new Error(request.getRequestURL().toString(), e));
         }
@@ -64,7 +42,7 @@ public class AdminController {
     @PostMapping("/admin")
     public ModelAndView connection(@Valid @ModelAttribute("model") Admin admin, BindingResult result, HttpSession session, HttpServletRequest request) {
         try {
-            if(result.hasErrors()) {
+            if (result.hasErrors()) {
                 return new ModelAndView("admin");
             }
             return new ModelAndView("redirect:/adminPanel/etudiant");
@@ -74,17 +52,15 @@ public class AdminController {
     }
 
     @GetMapping("/adminPanel/etudiant")
-    public ModelAndView showAdminPanelEtudiant(Model model)
-    {
+    public ModelAndView showAdminPanelEtudiant(Model model) {
         model.addAttribute("model", new Etudiant());
         return new ModelAndView("adminPanelFormEtudiant");
     }
 
     @PostMapping("/adminPanel/etudiant")
-    public ModelAndView ajouterEtudiant(@Valid @ModelAttribute("model") Etudiant etudiant, BindingResult result, HttpServletRequest request)
-    {
+    public ModelAndView ajouterEtudiant(@Valid @ModelAttribute("model") Etudiant etudiant, BindingResult result, HttpServletRequest request) {
         try {
-            if(result.hasErrors()) {
+            if (result.hasErrors()) {
                 return new ModelAndView("adminPanelFormEtudiant");
             }
             dataContext.listeEtudiant.add(etudiant);
@@ -95,8 +71,7 @@ public class AdminController {
     }
 
     @GetMapping("/adminPanel/cours")
-    public ModelAndView showAdminPanelCours(Model model)
-    {
+    public ModelAndView showAdminPanelCours(Model model) {
         int nextNum = dataContext.listeCours.size() + 1;
         Cours cours = new Cours();
         cours.setNumero(nextNum);
@@ -105,28 +80,13 @@ public class AdminController {
     }
 
     @PostMapping("/adminPanel/cours")
-    public ModelAndView ajouterCours(@Valid @ModelAttribute("model") Cours cours, BindingResult result, HttpServletRequest request)
-    {
+    public ModelAndView ajouterCours(@Valid @ModelAttribute("model") Cours cours, BindingResult result, HttpServletRequest request) {
         try {
-            if(result.hasErrors()) {
+            if (result.hasErrors()) {
                 return new ModelAndView("adminPanelFormCours");
             }
             dataContext.listeCours.add(cours);
             return new ModelAndView("redirect:/");
-        } catch (Exception e) {
-            return new ModelAndView("error", "model", new Error(request.getRequestURL().toString(), e));
-        }
-    }
-
-    @GetMapping("/adminPanel/logout")
-    public ModelAndView deconnectionAdminPanel(HttpServletRequest request, HttpSession session)
-    {
-        try {
-            if(session.getAttribute("admin") != null)
-            {
-                session.removeAttribute("admin");
-            }
-            return new ModelAndView("redirect:/admin");
         } catch (Exception e) {
             return new ModelAndView("error", "model", new Error(request.getRequestURL().toString(), e));
         }
